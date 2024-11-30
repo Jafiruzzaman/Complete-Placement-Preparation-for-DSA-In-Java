bda55cf20bf9aa70c22161ce9c9a1213fb7646a6
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 11:58:32 +0600

    Learn how to create a variables

---
+++ Java/src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+  public static void main(String[] args) {
+    System.out.println("variables in java");
+//  ! What is variables ?
+//  *  Ans : variables are containers to store values. Just like a bottle which contains water.
+    /*
+      ? Rules while creating variables
+    1. variables name can't start with Number or any other special characters
+    2. variables name must start with Letter's, underscore(_), and Dollar sign ($)
+    3. variables name can't be a keyword.
+    4. variables name should be meaningful and the best practice is use camelCase conventions
+     */
+    int age = 20;
+    System.out.println("age variables contains "+age);
+  }
+}
