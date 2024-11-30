2264a20c18681bf6039a16d59a25579e0aaa7eca
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 17:13:29 +0600

    Learn about for loop

---
+++ Java/src/forLoop.java
@@ -0,0 +1,8 @@
+public class forLoop {
+  public static void main(String[] args) {
+    System.out.println("Learn about for loop");
+    for (int j = 0; j < 10; j++) {
+      System.out.println("count : "+j);
+    }
+  }
+}
