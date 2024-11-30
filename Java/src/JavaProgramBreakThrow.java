b424cd504ad77cb5df223c9a55f486408853238d
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 11:43:54 +0600

    break down the java program

---
+++ Java/src/JavaProgramBreakThrow.java
@@ -0,0 +1,15 @@
+public class JavaProgramBreakThrow {
+  public static void main(String[] args) {
+    System.out.println("Java Program Break Throw");
+  }
+}
+/*
+public : public is an access modifier in Java
+class : class is a user defined blueprint
+JavaProgramBreakThrow : is the name the Java Program
+static : static allows to call methods without creating classes
+void : void is the return type. void means it doesn't return anything
+main : when the java program start it will start with the main method in java
+System.out.println() : when the program execute it will print what ever user give as an arguments
+
+ */
