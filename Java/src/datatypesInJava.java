3d39ddaf5515b7aa67555995c98057d768a2634d
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 12:09:25 +0600

    Primitive datatypes in Java

---
+++ Java/src/datatypesInJava.java
@@ -0,0 +1,39 @@
+public class datatypesInJava {
+  public static void main(String[] args) {
+    System.out.println("Datatypes in Java");
+//  * There are 8 types primitive of Data types in java
+//  1. byte
+//  2. short
+//  3. int
+//  4. long
+//  5. float
+//  6. double
+//  7. char
+//  8. boolean
+
+//  1. byte
+    byte shortestValue = -128;
+    System.out.println(shortestValue);
+//  2. short
+    short shortNumericValue = 32000;
+    System.out.println(shortNumericValue);
+//  3. int
+    int num = 652011;
+    System.out.println(num);
+//  4. long
+    long longestValue = 32541674165L;
+    System.out.println(longestValue);
+//  5. float
+    float weight = 65.24f;
+    System.out.println(weight);
+//  6. double
+    double height = 52.14515;
+    System.out.println(height);
+//  7. char
+    char a = 'a';
+    System.out.println(a);
+//  8. boolean
+    boolean isEatable = true;
+    System.out.println(isEatable);
+  }
+}
