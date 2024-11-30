06f0d1fcd858220930716d8e59629f2344eb5c75
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 17:37:56 +0600

    Learn About Methods

---
+++ Java/src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+  // Method declaration
+  public static void printHello(String name) {
+    System.out.println("Hello World with "+name);
+  }
+  public static void main(String[] args) {
+    System.out.println("Learn about Methods");
+    // Method invocation with parameters
+    printHello("Java");
+  }
+}
