b5d83ac073b0facb2708493eddc6c3e3db94db50
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 23:01:42 +0600

    solve reverse Number Problem

---
+++ Basics Programming Problems/src/reverseNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reverseNumber {
+  static int reverseNumber(int num){
+    int reversedNum = 0;
+    while(num!=0){
+      reversedNum = reversedNum * 10 + num % 10;
+      num = num / 10;
+    }
+    return reversedNum;
+  }
+  public static void main(String[] args) {
+    System.out.println("reverse a number");
+    System.out.println("Enter a number");
+    Scanner scanner = new Scanner(System.in);
+    int num = scanner.nextInt();
+    int reversedNum = reverseNumber(num);
+    System.out.println("the reverse number is " + reversedNum);
+  }
+}
