e9feb0cb412c2bdf0f93d22b56573c129f9618f8
Programmer-Jaf <programmerjaf@gmail.com>
2024-11-30 17:11:47 +0600

    Learn About While Loop

---
+++ Java/src/whileLoop.java
@@ -0,0 +1,10 @@
+public class whileLoop {
+  public static void main(String[] args) {
+    System.out.println("while loop");
+    int count = 0;
+    while (count < 10) {
+      System.out.println("Count: " + count);
+      count++;
+    }
+  }
+}
